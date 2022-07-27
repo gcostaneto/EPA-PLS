5335a56cc35aaa38d54248d7625a0bb4c51e7b15
Germano Costa Neto <germano.cneto@gmail.com>
2022-07-27 02:17:13 -0400

    Add files via upload

---
+++ EPA_PLS1.R
@@ -0,0 +1,9 @@
+#'---------------------------------------------------------------------------------------
+#'
+#' Title......: EPA Analysis using PLS 1 algorithm
+#' Goal.......: Compute genotype-specific reaction-norms
+#' Author.....: Germano Costa Neto
+#' Created at.: 2021-11-11
+#' Modified at: 2022-07-27
+#' 
+#'---------------------------------------------------------------------------------------
---
+++ EPA_PLS2.R
@@ -0,0 +1,9 @@
+#'---------------------------------------------------------------------------------------
+#'
+#' Title......: EPA Analysis using PLS 2 algorithm
+#' Goal.......: Compute environmental weights and model similarities for future locations
+#' Author.....: Germano Costa Neto
+#' Created at.: 2021-11-11
+#' Modified at: 2022-07-27
+#' 
+#'------------------------------------------------------------------------------------
